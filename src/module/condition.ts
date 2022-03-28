b4ead20751c6c148a2e7ae341e3e7f3f95c085b4
potensj@gmail.com <harry@intelliquant.net>
2022-03-29 00:17:50 +0900

    search

--- src/module/condition.ts
+++ src/module/condition.ts
@@ -3,9 +3,12 @@ import * as config from 'config';
 
 export async function ipAllowedCheck(ctx) {
   logger.info(`ip check`);
-  const trustedIps: Array<String> = config.get('ip-allowed');
+  //const trustedIps: Array<String> = config.get('ip-allowed');
   //test 용
-  //const trustedIps = "1.2.3.4.";
+  const trustedIps = [
+    "124.53.181.155",
+    "121.133.22.1"
+  ];
   logger.info(`ip: ${trustedIps}`);
   var requestIP = ctx.ip;
   logger.info(`ip-req: ${requestIP}`);
