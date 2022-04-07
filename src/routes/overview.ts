c35ff4e6702afde212777724f2331cc7f40f566c
potensj@gmail.com <harry@intelliquant.net>
2022-04-07 18:35:58 +0900

    white list

--- src/routes/overview.ts
+++ src/routes/overview.ts
@@ -54,6 +54,7 @@ router.get('/complain', async (ctx, next) => {
   const pageSignalResult = await complainDAO.getSpecificComplainData(paging.no, paging.page_size);
   const tableType = 'real';
   const forum = 'overview'
+  const pageType = 'normal';
   console.log(pageSignalResult);
   return ctx.render('complain', {pageSignalResult, paging, forum, tableType, moment});
 })
