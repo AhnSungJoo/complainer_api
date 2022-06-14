8b1c359d8c6a487b2471941375096c04a05448b1
potensj@gmail.com <harry@intelliquant.net>
2022-06-15 01:03:58 +0900

    new api server

--- src/routes/alarm.ts
+++ src/routes/alarm.ts
@@ -16,15 +16,13 @@ import {getPaging} from '../util/paging';
 import { config } from 'winston'
 
 // dao
-import singnalDAO from '../dao/signalDAO';
-import userDAO from '../dao/complainUserDAO';
-
+import kookminDAO from '../dao/kookminAlarmDAO';
 // condition
 import {ipAllowedCheck} from '../module/condition';
 
 const router: Router = new Router();
 
-// 기본정보입력
+// 알림등록
 router.post('/registerAlarm', async (ctx, next) => {
   logger.info('alarm');
   let toUserMsg = `신청서를 작성해주세요\n알림 신청서\n- 금액 :\n- 받는 날짜 :\n- 상대방 이름:\n- 상대방 번호 :\n`
@@ -40,6 +38,68 @@ router.post('/registerAlarm', async (ctx, next) => {
             ]
         }
     };
+  ctx.body = resutlJson;
+})
+
+// 신청서 작성
+router.post('/writeRegister', async (ctx, next) => {
+  logger.info('register complain');
+  const userId = ctx.request.body.userRequest.user.id;
+  let fromUserMsg = ctx.request.body.userRequest.utterance;;
+  let toUserMsg = '';
+  logger.info(`${fromUserMsg}`);
+  logger.info(`userid: ${userId}`);
+  let resutlJson;
+  if(fromUserMsg.trim().indexOf('신청서') != -1) {
+    logger.info('alarm');
+    try {
+      const kookDAO = new kookminDAO();
+      await kookDAO.insertKookminApply(userId, fromUserMsg);
+      toUserMsg = `신청서 작성이 완료됐습니다.`
+      resutlJson = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": toUserMsg
+                        }
+                    }
+                ]
+            }
+        }; 
+    } catch(err) {
+      toUserMsg = `신청서 작성 중 오류가 발생했습니다.\n다시 시도해주세요.`
+      resutlJson = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": toUserMsg
+                        }
+                    }
+                ]
+            }
+        }; 
+    }
+     
+  }
+  else {
+    toUserMsg = `신청서를 다시 작성해주세요.`
+    resutlJson = {
+          "version": "2.0",
+          "template": {
+              "outputs": [
+                  {
+                      "simpleText": {
+                          "text": toUserMsg
+                      }
+                  }
+              ]
+          }
+      };     
+  }
   ctx.body = resutlJson;
 })
 export default router;
