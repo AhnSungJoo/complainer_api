400387d2243810ed9a93ee692e882466fc64744d
potensj@gmail.com <harry@intelliquant.net>
2022-06-17 21:08:15 +0900

    new

--- src/dao/kookminAlarmDAO.ts
+++ src/dao/kookminAlarmDAO.ts
@@ -5,15 +5,36 @@ import * as moment from 'moment'
 
 export default class complainUserDAO extends MySqlDAO {
   constructor() {
-    super('real-mysql', 'kookmin_alarm_apply'); 
+    super('real-mysql', 'kookmin_alarm'); 
   }
 
   getAllComplainerData() {
     return this.get();
   }
 
-  insertKookminApply(userId, msg) {
-    const query: string = `insert into ${this.table} (kakao_id, contents) values ('${userId}', '${msg}')`;
+  insertKookminMoney(userId, money) {
+    const query: string = `insert into ${this.table} (kakao_id, money_amount, alarm_agree) values ('${userId}', '${money}', 0)`;
+    logger.info(`query: ${query}`);
+    return DBHelper.query(this.targetDB, query)
+    .then((data: any) => data.result);
+  }
+
+  updateKookminDate(userId, receive_date) {
+    const query: string = `UPDATE ${this.table} SET receive_date = '${receive_date}' WHERE kakao_id = '${userId}' and alarm_agree = 0`;
+    logger.info(`query: ${query}`);
+    return DBHelper.query(this.targetDB, query)
+    .then((data: any) => data.result);
+  }
+
+  updateKookminReceive(userId, name, phoneNumber) {
+    const query: string = `UPDATE ${this.table} SET user_name = '${name}', user_phone_number='${phoneNumber}' WHERE kakao_id = '${userId}' and alarm_agree = 0`;
+    logger.info(`query: ${query}`);
+    return DBHelper.query(this.targetDB, query)
+    .then((data: any) => data.result);
+  }
+
+  updateKookminBorrow(userId, name, phoneNumber) {
+    const query: string = `UPDATE ${this.table} SET other_user_name = '${name}', other_phone_number='${phoneNumber}' WHERE kakao_id = '${userId}' and alarm_agree = 0`;
     logger.info(`query: ${query}`);
     return DBHelper.query(this.targetDB, query)
     .then((data: any) => data.result);
--- src/routes/alarm.ts
+++ src/routes/alarm.ts
@@ -25,39 +25,7 @@ const router: Router = new Router();
 // 알림등록
 router.post('/registerAlarm', async (ctx, next) => {
   logger.info('alarm');
-  let toUserMsg = `신청서를 작성해주세요\n알림 신청서\n- 금액 :\n- 받는 날짜 :\n- 상대방 이름:\n- 상대방 번호 :\n`
-  let resutlJson = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": toUserMsg
-                    }
-                }
-            ]
-        }
-    };
-  ctx.body = resutlJson;
-})
-
-// 알림등록
-router.post('/dateCheck', async (ctx, next) => {
-  logger.info('dateCheck');
-  logger.info(`${ctx.request.body.user.id}`);
-  logger.info(`${ctx.request.body.value.origin}`);
-
-  return ctx.body = {status: 'success'};
-})
-
-// 알림등록
-router.post('/registerDate', async (ctx, next) => {
-  logger.info('registerDate');
-  let toUserMsg = `신청서를 작성해주세요\n알림 신청서\n- 금액 :\n- 받는 날짜 :\n- 상대방 이름:\n- 상대방 번호 :\n`
-  const userId = ctx.request.body.userRequest.user.id;
-  let fromUserMsg = ctx.request.body.userRequest.utterance;
-  logger.info(`${fromUserMsg}`);
-  logger.info(`${userId}`);
+  let toUserMsg = `이자 포함 얼마를 받으셔야 하나요? (양식: 00원)`
   let resutlJson = {
         "version": "2.0",
         "template": {
@@ -81,32 +49,24 @@ router.post('/writeRegister', async (ctx, next) => {
   logger.info(`${fromUserMsg}`);
   logger.info(`userid: ${userId}`);
   let resutlJson;
-  if(fromUserMsg.trim().indexOf('신청서') != -1) {
-    logger.info('alarm');
+  if(fromUserMsg.trim().indexOf('원') != -1) {
     try {
+      fromUserMsg = await refineMsg(fromUserMsg);
       const kookDAO = new kookminDAO();
-      await kookDAO.insertKookminApply(userId, fromUserMsg);
-      let finalMsg = fromUserMsg.replace("신청서를 작성해주세요", "");
-      toUserMsg = `신청서 작성이 완료됐습니다. 신청내역을 다시 한번 확인 후 '확인 및 동의 버튼'을 눌러주세요!\n ${finalMsg}`;
-        resutlJson = {
-          "version": "2.0",
-          "template": {
-              "outputs": [
-                  {
-                      "simpleText": {
-                          "text": toUserMsg
-                      }
-                  }
-              ],
-              "quickReplies": [
+      await kookDAO.insertKookminMoney(userId, fromUserMsg);
+      toUserMsg = `언제까지 받기로 약속하셨나요? (양식: 00년 00월 00일)`;
+      resutlJson = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
                 {
-                  "messageText": "확인 및 동의",
-                  "action": "message",
-                  "label": "확인 및 동의"
+                    "simpleText": {
+                        "text": toUserMsg
+                    }
                 }
-              ]
-          }
-        };
+            ]
+        }
+    };
     } catch(err) {
       toUserMsg = `신청서 작성 중 오류가 발생했습니다.\n다시 시도해주세요.`
       resutlJson = {
@@ -122,22 +82,118 @@ router.post('/writeRegister', async (ctx, next) => {
             }
         }; 
     }
-     
   }
-  else if(fromUserMsg.trim().indexOf('확인 및 동의') != -1) {
-    toUserMsg = `신청서 작성이 완료됐습니다.\n알람은 하루전, 삼일전, 일주일전, 이주일전, 한달전 주기로 발송됩니다.`
-    resutlJson = {
-          "version": "2.0",
-          "template": {
-              "outputs": [
-                  {
-                      "simpleText": {
-                          "text": toUserMsg
-                      }
-                  }
-              ]
-          }
-      };     
+  else if(fromUserMsg.trim().indexOf('년') != -1) {
+    try {
+      fromUserMsg = await refineMsg(fromUserMsg);
+      const kookDAO = new kookminDAO();
+      await kookDAO.updateKookminDate(userId, fromUserMsg);
+      toUserMsg = `빌려주신 분의 이름과 번호를 알려주세요 (양식: 빌려준분: 안성주, 번호: 01012345678) `;
+      resutlJson = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": toUserMsg
+                    }
+                }
+            ]
+        }
+    };
+    } catch(err) {
+      toUserMsg = `신청서 작성 중 오류가 발생했습니다.\n다시 시도해주세요.`
+      resutlJson = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": toUserMsg
+                        }
+                    }
+                ]
+            }
+        }; 
+    }
+  }
+  else if(fromUserMsg.trim().indexOf('빌려준분') != -1) {
+    try {
+      fromUserMsg = await refineMsg(fromUserMsg);
+      let startIdx = fromUserMsg.indexOf('빌려준분');
+      let endIdx = fromUserMsg.indexOf(',');
+      let name = fromUserMsg.substring(startIdx, endIdx);
+      startIdx = fromUserMsg.indexOf('번호:');
+      let phoneNumber = fromUserMsg.substring(startIdx, -1);
+      const kookDAO = new kookminDAO();
+      await kookDAO.updateKookminReceive(userId, name, phoneNumber);
+      toUserMsg = `갚으시는 분의 이름과 번호를 알려주세요 (양식: 갚는분: 안성주, 번호: 01012345678) `;
+      resutlJson = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": toUserMsg
+                    }
+                }
+            ]
+        }
+    };
+    } catch(err) {
+      toUserMsg = `신청서 작성 중 오류가 발생했습니다.\n다시 시도해주세요.`
+      resutlJson = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": toUserMsg
+                        }
+                    }
+                ]
+            }
+        }; 
+    }
+  }
+  else if(fromUserMsg.trim().indexOf('갚는분') != -1) {
+    try {
+      fromUserMsg = await refineMsg(fromUserMsg);
+      let startIdx = fromUserMsg.indexOf('갚는분');
+      let endIdx = fromUserMsg.indexOf(',');
+      let name = fromUserMsg.substring(startIdx, endIdx);
+      startIdx = fromUserMsg.indexOf('번호:');
+      let phoneNumber = fromUserMsg.substring(startIdx, -1);
+      const kookDAO = new kookminDAO();
+      await kookDAO.updateKookminBorrow(userId, name, phoneNumber);
+      toUserMsg = `감사합니다. 갚으시는 분의 확인을 받은 후 간단한 서류와 알람을 설정해드리겠습니다.`;
+      resutlJson = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": toUserMsg
+                    }
+                }
+            ]
+        }
+    };
+    } catch(err) {
+      toUserMsg = `신청서 작성 중 오류가 발생했습니다.\n다시 시도해주세요.`
+      resutlJson = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": toUserMsg
+                        }
+                    }
+                ]
+            }
+        }; 
+    }
   }
   else {
     toUserMsg = `신청서를 다시 작성해주세요.`
@@ -156,4 +212,21 @@ router.post('/writeRegister', async (ctx, next) => {
   }
   ctx.body = resutlJson;
 })
+
+
+// 양식 및 괄호 제거
+async function refineMsg(msg) {
+  msg = msg.trim();
+  if(msg.indexOf('양식:') != -1) {
+    msg = msg.replace("양식:", "");
+  }
+  if(msg.indexOf('(') != -1) {
+    msg = msg.replace("(", "");
+  }
+  if(msg.indexOf(')') != -1) {
+    msg = msg.replace(")", "");
+  }
+  return msg;
+}
+
 export default router;
