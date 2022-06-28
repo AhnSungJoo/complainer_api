0cb18cd740494ece7cce1b6f7ffb0c13c6d01d26
potensj@gmail.com <harry@intelliquant.net>
2022-06-28 20:27:23 +0900

    new page

--- src/dao/albaReviewDAO.ts
+++ src/dao/albaReviewDAO.ts
@@ -8,22 +8,22 @@ export default class complainUserDAO extends MySqlDAO {
     super('real-mysql', 'alba_review'); 
   }
   insertAlbaReview(userId, address) {
-    const query: string = `insert into ${this.table} (kakao_id, alba_addres) values ('${userId}', '${address}')`;
+    const query: string = `insert into ${this.table} (kakao_id, alba_address) values ('${userId}', '${address}')`;
     logger.info(`${query}`);
     return DBHelper.query(this.targetDB, query)
     .then((data: any) => data.result);
   }
 
   updateAlbaCompany(userId, companyName) {
-    const query: string = `UPDATE ${this.table} SET alba_company = '${companyName}' WHERE kakao_id = '${userId}'`;
-
+    const query: string = `UPDATE ${this.table} SET alba_company = '${companyName}' WHERE kakao_id = '${userId}' and register_complete = 0`;
+    logger.info(`${query}`);
     return DBHelper.query(this.targetDB, query)
     .then((data: any) => data.result);
   }
   
   updateAlbaReview(userId, review_content) {
-    const query: string = `UPDATE ${this.table} SET alba_ = '${review_content}' WHERE kakao_id = '${userId}'`;
-
+    const query: string = `UPDATE ${this.table} SET alba_ = '${review_content}', register_complete = 1 WHERE kakao_id = '${userId}' and register_complete = 0`;
+    logger.info(`${query}`);
     return DBHelper.query(this.targetDB, query)
     .then((data: any) => data.result);
   }
