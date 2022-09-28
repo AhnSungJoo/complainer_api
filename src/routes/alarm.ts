6d10fce5ce61b36270c011e804dd589daa7b8897
potensj@gmail.com <harry@intelliquant.net>
2022-09-28 13:40:12 +0900

    add proxy

--- src/routes/alarm.ts
+++ src/routes/alarm.ts
@@ -5,6 +5,8 @@ import * as moment from 'moment';
 import * as settingConfig from 'config';
 //import * as Slack from 'slack-node';
 import * as Slack from "@slack/webhook";
+import HttpsProxyAgent from 'https-proxy-agent';
+
 // import * as emoji from 'telegram-emoji-map';
 
 import logger from '../util/logger';
@@ -24,7 +26,9 @@ import kookminUserDAO from '../dao/kookminUserDAO';
 import {ipAllowedCheck} from '../module/condition';
 
 const router: Router = new Router();
-const webhook = new Slack.IncomingWebhook("https://hooks.slack.com/services/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");
+const proxy = new HttpsProxyAgent(process.env.http_proxy || 'http://23.20.108.250:8080');
+const slackUrl = "https://hooks.slack.com/services/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";
+const webhook = new Slack.IncomingWebhook(slackUrl, { agent: proxy });
 
 // 알림등록
 router.post('/registerAlarm', async (ctx, next) => {
