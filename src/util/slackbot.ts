688743e2150bed15ac1f6f707403b17492b75fdd
potensj@gmail.com <harry@intelliquant.net>
2022-10-05 22:15:02 +0900

    plz

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -1,26 +1,24 @@
 'use strict';
 import logger from '../util/logger';
-
+import * as request from 'request';
 
 export async function sendSlackWebHook() {
   try {
-  const { IncomingWebhook } = require('@slack/client');
-  const url = "https://hooks.slack.com/services/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";
-
-  const webhook = new IncomingWebhook(url);
-  let data = "hi there"
-    logger.info(`webhook : ${JSON.stringify(webhook)}`);
-    logger.info(data);
-    // Send simple text to the webhook channel
-    webhook.send(data, function(err, res) {
-      logger.info('send');
+    const url = "https://hooks.slack.com/services/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";
+      //console.log('fullUrl : ' + fullUrl);
+      return new Promise((resolve, reject) => {
+        request(url, (err, res, body) => {
           if (err) {
-              logger.info(`Error: ${err}`);
-          } else {
-              logger.info('Message sent: ', res);
+            logger.info(err);
           }
-    });
-  } catch(err) {
-    logger.info(err);
-  }
+          else {
+            resolve();
+          }
+          // return res && res.statusCode
+          // console.log('TelegramUtil : statusCode=', res && res.statusCode);
+        });
+      })
+    } catch(err) {
+      logger.info(err);
+    }
 }
