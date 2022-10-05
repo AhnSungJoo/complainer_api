920b1693febfe28e4ba201325c1208c1d00ec66b
potensj@gmail.com <harry@intelliquant.net>
2022-10-05 21:42:18 +0900

    find issue

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -3,9 +3,10 @@ import logger from '../util/logger';
 
 
 export async function sendSlackWebHook() {
+  try {
   const { IncomingWebhook } = require('@slack/client');
   const url = "https://hooks.slack.com/services/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";
-  try {
+
   const webhook = new IncomingWebhook(url);
   let data = "hi there"
 
