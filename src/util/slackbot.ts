221ad1159d00fb3607557a6e58a329251b9bc938
potensj@gmail.com <harry@intelliquant.net>
2022-10-05 21:35:21 +0900

    add logger

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -7,13 +7,16 @@ export function sendSlackWebHook() {
   const url = "https://hooks.slack.com/services/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";
   const webhook = new IncomingWebhook(url);
   let data = "hi there"
-  // Send simple text to the webhook channel
-  webhook.send(data, function(err, res) {
-      if (err) {
-          logger.info(`Error: ${err}`);
-      } else {
-          console.log('Message sent: ', res);
-      }
-  });
-  
+  try {
+      // Send simple text to the webhook channel
+      webhook.send(data, function(err, res) {
+            if (err) {
+                logger.info(`Error: ${err}`);
+            } else {
+                console.log('Message sent: ', res);
+            }
+      });
+  } catch(err) {
+    logger.info(err);
+  }
 }
