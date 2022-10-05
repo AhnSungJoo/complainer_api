99cb601a2eff52dcdd7a08da6ebccc9eb368a1ca
potensj@gmail.com <harry@intelliquant.net>
2022-10-05 18:21:26 +0900

    change

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1047,11 +1047,9 @@ router.post('/kakaoChat/getMyRefCode', async (ctx, next) => {
 })
 
 // 추천인 코드  생성
-async function generateRefCode() {
-  logger.info("is it okay? here22?");
+async function generateRefCode() 
   try {
   let CodeGenerator = require('node-code-generator');
-  logger.info("is it okay? here?");
   // DB던 어디던 기존의 모든 추천인코드를 일단 한번에 다 가져오고, 그 목록을 code generator에게 넘겨주고 그 generator가 알아서 중복되지 않는 코드를 생성하게 함.
   return new complainUserDAO().get()
   .then(async userSet => {
