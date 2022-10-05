a41f7a59f025f858ea0949da28631aaee4cc986e
potensj@gmail.com <harry@intelliquant.net>
2022-10-05 22:50:57 +0900

    change

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -5,28 +5,20 @@ import * as request from 'request';
 export async function sendSlackWebHook() {
   try {
     const url = "https://hooks.slack.com/services/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";
-
-    let options = {
-      uri: url,
-      method: 'POST',
-      body:{
-        "text" : "gogo"
-      },
-      json:true //json으로 보낼경우 true로 해주어야 header값이 json으로 설정됩니다.
-  };
-      return new Promise((resolve, reject) => {
-        request(options, (err, res, body) => {
-          if (err) {
-            logger.info(err);
-          }
-          else {
-            resolve();
-          }
-          // return res && res.statusCode
-          // console.log('TelegramUtil : statusCode=', res && res.statusCode);
-          logger.info(res.statusCode);
-        });
-      })
+      request({
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        uri: url,
+        body:{
+          "text" : "gogo"
+        },
+        method: 'POST',
+        json: true
+      }, function (err, res, body) {
+        //it works!
+        logger.info(`res: ${res.statusCode}`);
+      });
     } catch(err) {
       logger.info(err);
     }
