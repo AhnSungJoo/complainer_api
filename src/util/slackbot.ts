386e05d57d52704ee354b18732bcf95ccde64b4e
potensj@gmail.com <harry@intelliquant.net>
2022-10-05 23:01:20 +0900

    plz

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -2,23 +2,33 @@
 import logger from '../util/logger';
 import * as request from 'request';
 
-export function sendSlackWebHook() {
+export async function sendSlackWebHook() {
   try {
     const url = "https://hooks.slack.com/services/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";
-      request({
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        url: url,
-        body:{
-          "text" : "gogo"
-        },
-        method: 'POST',
-        json: true
-      }, function (err, res, body) {
-        //it works!
+      r   //console.log('fullUrl : ' + fullUrl);
+      return new Promise((resolve, reject) => {
+        request({
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          url: url,
+          body:{
+            "text" : "gogo"
+          },
+          method: 'POST',
+          json: true
+        }, (err, res, body) => {
+          if (err) {
+            logger.info(err);
+          }
+          else {
+            resolve();
+          }
+          // return res && res.statusCode
+          // console.log('TelegramUtil : statusCode=', res && res.statusCode);
+        });
         logger.info(`res: ${res.statusCode}`);
-      });
+      })
     } catch(err) {
       logger.info(err);
     }
