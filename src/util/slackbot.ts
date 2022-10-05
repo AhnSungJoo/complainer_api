cb54aae22a7a94721e0dd936cf151bedcba4c5b1
potensj@gmail.com <harry@intelliquant.net>
2022-10-05 21:46:46 +0900

    change url

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -5,7 +5,7 @@ import logger from '../util/logger';
 export async function sendSlackWebHook() {
   try {
   const { IncomingWebhook } = require('@slack/client');
-  const url = "https://hooks.slack.com/services/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";
+  const url = "https://hooks.slack.com/services/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";
 
   const webhook = new IncomingWebhook(url);
   let data = "hi there"
