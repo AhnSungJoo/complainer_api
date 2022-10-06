b8b5489d1c5efc4be797f593e2222df6dedda9e6
potensj@gmail.com <harry@intelliquant.net>
2022-10-06 14:46:51 +0900

    slack node

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -3,6 +3,7 @@ import * as request from 'request';
 
 export async function sendSlackWebHook() {
   try {
+    /*
     const url = "https://hooks.slack.com/services/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";
       //console.log('fullUrl : ' + fullUrl);
       return new Promise((resolve, reject) => {
@@ -31,4 +32,19 @@ export async function sendSlackWebHook() {
     } catch(err) {
       logger.info(err);
     }
+    */
+   let Slack = require('slack-node');
+
+  const wecbhookUri = "https://hooks.slack.com/services/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";
+
+  const slack = new Slack();
+  slack.setWebhook(wecbhookUri);
+
+  slack.webhook({
+      channel: "#봇테스트",	// 현 슬랙의 채널
+      username: "불편이", // 슬랙에서 보여질 웹훅 이름
+      text: "테스트222"	//텍스트
+  }, function (err, response) {
+      console.log(response);
+  });
 }
