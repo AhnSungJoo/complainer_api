9de83b9d99e5b505c4b6bba4c3a16b869ee2390a
potensj@gmail.com <harry@intelliquant.net>
2022-10-06 12:21:44 +0900

    url

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -4,7 +4,7 @@ import * as request from 'request';
 
 export async function sendSlackWebHook() {
   try {
-    const url = "https://hooks.slack.com/services/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";
+    const url = "https://hooks.slack.com/services/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";
       //console.log('fullUrl : ' + fullUrl);
       return new Promise((resolve, reject) => {
         request({
