27b9a20c866320d53b491c1457d6e614d272ff3b
potensj@gmail.com <harry@intelliquant.net>
2022-10-06 14:58:02 +0900

    plz

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -3,13 +3,14 @@ import * as request from 'request';
 
 export async function sendSlackWebHook() {
   try {
-    /*
+    const slackToken = `xoxb-4033740111041-4179044897829-mNGwmTogdP7Y6qq4LnohzK65`;
     const url = "https://hooks.slack.com/services/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";
       //console.log('fullUrl : ' + fullUrl);
       return new Promise((resolve, reject) => {
         request({
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'authorization' : `Bearer ${slackToken}`
           },
           url: url,
           body:{
@@ -32,22 +33,4 @@ export async function sendSlackWebHook() {
     } catch(err) {
       logger.info(err);
     }
-    */
-   let Slack = require('slack-node');
-
-  const wecbhookUri = "https://hooks.slack.com/services/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";
-
-  const slack = new Slack();
-  slack.setWebhook(wecbhookUri);
-
-  slack.webhook({ 
-      channel: "#봇테스트",	// 현 슬랙의 채널
-      username: "불편이", // 슬랙에서 보여질 웹훅 이름
-      text: "테스트222"	//텍스트
-  }, function (err, response) {
-      console.log(response);
-  });
-  } catch(err) {
-    logger.info(err);
-  }
 }
