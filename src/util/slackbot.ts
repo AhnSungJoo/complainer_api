84228b921ee6b3771bea1dc133531b9bb4ff3caf
potensj@gmail.com <harry@intelliquant.net>
2022-10-08 18:47:46 +0900

    chagne

--- src/util/slackbot.ts
+++ src/util/slackbot.ts
@@ -3,7 +3,7 @@ import * as slackHook from '@slack/webhook';
  
 export async function sendSlackWebHook(msg) {
   try {
-    const url = "https://hooks.slack.com/services/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";
+    const url = "https://hooks.slack.com/services/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";
     const webhook = new slackHook.IncomingWebhook(url);
 
     await webhook.send({
