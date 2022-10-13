0039e9cbf80b7e29acf4335bf2f8bd932ff6d204
potensj@gmail.com <harry@intelliquant.net>
2022-10-13 16:42:43 +0900

    chagne

--- src/util/slackconfig.ts
+++ src/util/slackconfig.ts
@@ -1,4 +1,4 @@
-let baseUrl = "https://hooks.slack.com/services/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";
+let baseUrl = "https://hooks.slack.com/services/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";
 let moneyUrl = "https://hooks.slack.com/services/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";
 
 export function returnURL(botType) {
@@ -7,7 +7,5 @@ export function returnURL(botType) {
   }
   else if(botType == "money") {
     return moneyUrl;
-  }
-    
+  }   
 }
-https://hooks.slack.com/services/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
