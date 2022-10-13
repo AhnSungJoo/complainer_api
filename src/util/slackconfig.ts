7d2dc22aa3212f9d06d47aef0c27c9b1f062b4f3
potensj@gmail.com <harry@intelliquant.net>
2022-10-13 16:37:44 +0900

    change

--- src/util/slackconfig.ts
+++ src/util/slackconfig.ts
@@ -1,6 +1,9 @@
-let baseUrl = "https://hooks.slack.com/services/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";
+let baseUrl = "https://hooks.slack.com/services/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";
+
+//https://hooks.slack.com/services/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
 //let baseUrl =  "https://hooks.slack.com/services/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" // 불편이봇 
-let moneyUrl = "https://hooks.slack.com/services/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"; //얼마빌렸지 봇
+let moneyUrl = "https://hooks.slack.com/services/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";
+//let moneyUrl = "https://hooks.slack.com/services/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"; //얼마빌렸지 봇
 
 export function returnURL(botType) {
   if(botType == "complain") {
