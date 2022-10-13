c80208e5f0a3a35bd43f0fc4f9e5e10fcf5f3469
potensj@gmail.com <harry@intelliquant.net>
2022-10-13 16:41:08 +0900

    chagne

--- src/util/slackconfig.ts
+++ src/util/slackconfig.ts
@@ -1,5 +1,5 @@
 let baseUrl = "https://hooks.slack.com/services/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";
-let moneyUrl = "https://hooks.slack.com/services/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";
+let moneyUrl = "https://hooks.slack.com/services/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";
 
 export function returnURL(botType) {
   if(botType == "complain") {
