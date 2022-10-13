4e16d76fe7d1dae6874e02396f17086e56ea59de
potensj@gmail.com <harry@intelliquant.net>
2022-10-13 16:34:58 +0900

    change

--- src/util/slackconfig.ts
+++ src/util/slackconfig.ts
@@ -1,4 +1,5 @@
-let baseUrl =  "https://hooks.slack.com/services/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" // 불편이봇 
+let baseUrl = "https://hooks.slack.com/services/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";
+//let baseUrl =  "https://hooks.slack.com/services/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" // 불편이봇 
 let moneyUrl = "https://hooks.slack.com/services/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"; //얼마빌렸지 봇
 
 export function returnURL(botType) {
