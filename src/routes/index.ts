c73ddf9a9f9e02a7342b27224733326091e86226
potensj@gmail.com <harry@intelliquant.net>
2022-10-20 11:26:21 +0900

    change

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -460,9 +460,10 @@ router.post('/kakaoChat/reqIncome', async (ctx, next) => {
       }
       else {
         await complainerDAO.updateComplainUserIncome(userId);
-        toUserMsg = `💰현재 적립 포인트 : “${totalPointComma}”원
-출금신청이 완료됐습니다.
-* 본인 확인을 위해 아래 "상담직원 연결"을 누르신 후 메시지를 보내주세요. 감사합니다.`;
+        toUserMsg = `👩🏻 출금신청이 접수되었습니다.
+      💰 출금 예정 금액 : “3,000”원
+      ✔️ 본인 확인을 위해 아래 “상담직원 연결”
+               메뉴를 누르신 후 메시지를 보내주세요. `;
       await sendSlackWebHook(`💰 “프로불편러”에 출금신청 완료!`, 'complain');
       }
 
