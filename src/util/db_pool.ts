b26423d9f53eb27369e4cb6af63a6fc40dd8c3d8
harry <potensj@gmail.com>
2022-11-16 20:42:51 +0900

    Change

--- src/util/db_pool.ts
+++ src/util/db_pool.ts
@@ -21,8 +21,8 @@ class DBPool {
        this.pool[key] = mysql.createPool({
         connectionLimit: 1000,
         host: "127.0.0.1",
-        user: "root",
-        password: "potensj11",
+        user: "complainer",
+        password: "#potensj11",
         database: "complainer"
       });
 
